d83055c1a7060da83c67a90ae680eb7fadaa5ce7
cngregory <cngregory@miners.utep.edu>
2026-06-14 16:34:31 -0600

    Initial Dart project setup

---
+++ bin/cs4381_game_b.dart
@@ -0,0 +1,5 @@
+import 'package:cs4381_game_b/cs4381_game_b.dart' as cs4381_game_b;
+
+void main(List<String> arguments) {
+  print('Hello world: ${cs4381_game_b.calculate()}!');
+}
---
+++ test/cs4381_game_b_test.dart
@@ -0,0 +1,8 @@
+import 'package:cs4381_game_b/cs4381_game_b.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
